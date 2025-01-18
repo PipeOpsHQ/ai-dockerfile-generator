e4e95f544ea3ae99640339dab023318897ee0dd7
Alex Idowu <alexidowu25@gmail.com>
2025-01-18 21:22:39 +0100

    feat: Add react-helmet-async for improved SEO capabilities

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './App.css'
-
-// import './generator.css'
-// import './custom.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
+import App from './App.jsx';
+import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
+);
