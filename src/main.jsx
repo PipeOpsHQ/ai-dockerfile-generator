6111b28230033339dded5fa843cfc890c6230785
Alex Idowu <alexidowu25@gmail.com>
2025-01-18 20:17:42 +0100

    style: Update CSS styles in App.css for better readability and organization

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
-import './generator.css'
-import './custom.css'
+import './App.css'
+
+// import './generator.css'
+// import './custom.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
